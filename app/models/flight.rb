b3b81de533796cd72b0ff4ec4a01dd0e71da9ce9
Arjun Venkataswamy <avenkat2@gmail.com>
2013-05-20 15:02:08 -0500

    creates method to calculate flight mileage

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,3 +1,20 @@
 class Flight < ActiveRecord::Base
   attr_accessible :arrival_airport, :departs_at, :departure_airport, :distance, :flight_num, :seats
+
+  def miles
+    if departure_airport == 'ORD' && arrival_airport == 'JFK'
+      return 740
+    elsif departure_airport == 'ORD' && arrival_airport == 'LAX'
+      return 1744
+    elsif departure_airport == 'ORD' && arrival_airport == 'SFO'
+      return 1846
+    elsif departure_airport == 'JFK' && arrival_airport ==
+      'LAX'
+      return 2475
+    elsif departure_airport == 'JFK' && arrival_airport == 'SFO'
+      return 2586
+    elsif departure_airport == 'LAX' && arrival_airport == 'SFO'
+      return 338
+    end
+  end
 end
